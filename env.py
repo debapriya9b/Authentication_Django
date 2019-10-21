e37c84be3c3e1abc50e3cdae3ef3dd91b02155bc
Ubuntu <ubuntu@ip-172-31-80-240.ec2.internal>
2019-10-21 05:52:50 +0000

    Authentication with email id created

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os 
 os.environ.setdefault("EMAIL_ADDRESS", "debapriya9b@gmail.com")
-os.environ.setdefault("EMAIL_PASSWORD", "mypassword") 
+os.environ.setdefault("EMAIL_PASSWORD", "Advik_123456") 
